d6fcc57b2706939ef2c4918d3b5fea06120f117f
David Livingstone <david@david.tl>
2015-12-07 11:43:18 -0800

    set up store objects

---
+++ js/sales.js
@@ -0,0 +1,31 @@
+// global variables
+var pikePlace = {
+  location: 'Pike Place Market',
+  min: 17,
+  max: 88,
+  avg: 5.2,
+};
+var seaTac = {
+  location: 'SeaTac Airport',
+  min: 6,
+  max: 44,
+  avg: 1.2,
+};
+var southCenter = {
+  location: 'Southcenter Mall',
+  min: 11,
+  max: 38,
+  avg: 1.9,
+};
+var bellevueSquare = {
+  location: 'Bellevue Square',
+  min: 20,
+  max: 48,
+  avg: 3.3,
+};
+var alki = {
+  location: 'Alki',
+  min: 3,
+  max: 24,
+  avg: 26,
+};
