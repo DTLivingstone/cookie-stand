f22d32b8cee908284144d9034d55dd909a8b3000
David Livingstone <david@david.tl>
2015-12-07 17:11:54 -0800

    added functions for hourly volume and daily volume

--- js/sales.js
+++ js/sales.js
@@ -1,31 +1,84 @@
-// global variables
+// initialize  store objects
 var pikePlace = {
   location: 'Pike Place Market',
-  min: 17,
-  max: 88,
-  avg: 5.2,
-};
+  minHourlyCustomers: 17,
+  maxHourlyCustomers: 88,
+  avgCustomerVolume: 5.2,
+  hourlyVolume: [],
+  dailyVolume: 0,
+}
 var seaTac = {
   location: 'SeaTac Airport',
-  min: 6,
-  max: 44,
-  avg: 1.2,
-};
-var southCenter = {
-  location: 'Southcenter Mall',
-  min: 11,
-  max: 38,
-  avg: 1.9,
-};
+  minHourlyCustomers: 6,
+  maxHourlyCustomers: 44,
+  avgCustomerVolume: 1.2,
+  hourlyVolume: [],
+  dailyVolume: 0,
+}
+var southcenter = {
+  location: 'southcenter Mall',
+  minHourlyCustomers: 11,
+  maxHourlyCustomers: 38,
+  avgCustomerVolume: 1.9,
+  hourlyVolume: [],
+  dailyVolume: 0,
+}
 var bellevueSquare = {
   location: 'Bellevue Square',
-  min: 20,
-  max: 48,
-  avg: 3.3,
-};
+  minHourlyCustomers: 20,
+  maxHourlyCustomers: 48,
+  avgCustomerVolume: 3.3,
+  hourlyVolume: [],
+  dailyVolume: 0,
+}
 var alki = {
   location: 'Alki',
-  min: 3,
-  max: 24,
-  avg: 26,
-};
+  minHourlyCustomers: 3,
+  maxHourlyCustomers: 24,
+  avgCustomerVolume: 26,
+  hourlyVolume: [],
+  dailyVolume: 0,
+}
+
+// class examples
+// function Student(course, lastInitial, hairColor) {
+//   this.course = course;
+//   this.lastInitial = lastInitial;
+//   this.hairColor = hairColor;
+// }
+//
+// // these exist outside, isn't in memory until called
+// Student.prototype.intro = function() {
+//   console.log('My last initial is' + this.lastInitial);
+// };
+//
+// Student.prototype.location = 'Washington';
+//
+// var paul = new Student('201d2', 'S', 'auburn');
+
+// functions
+function calcHourlyVolume (store) {
+   for (var i = 0; i <= 7; i++) {
+    store.hourlyVolume[i] = Math.round((Math.random() * (store.maxHourlyCustomers - store.minHourlyCustomers) + store.minHourlyCustomers) * store.avgCustomerVolume);
+  }
+}
+
+function calcDailyVolume (store) {
+  for (var i = 0; i <= 7; i++) {
+    store.dailyVolume = store.dailyVolume + store.hourlyVolume[i];
+  }
+}
+
+// calculate hourly volume
+calcHourlyVolume (pikePlace);
+calcHourlyVolume (seaTac);
+calcHourlyVolume (southcenter);
+calcHourlyVolume (bellevueSquare);
+calcHourlyVolume (alki);
+
+// calculate daily volume
+calcDailyVolume (pikePlace);
+calcDailyVolume (seaTac);
+calcDailyVolume (southcenter);
+calcDailyVolume (bellevueSquare);
+calcDailyVolume (alki);
