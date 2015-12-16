eb8b1fe534398f2d21cd4482d4c9e4dd96c69f08
David Livingstone <david@david.tl>
2015-12-16 11:14:07 -0800

    calcHourlyVolume and calcDailyVolume prototypes working with dummy values

--- js/sales.js
+++ js/sales.js
@@ -1,6 +1,6 @@
 // constructor and prototypes
 
-var Store = function(site, minCustomers, maxCustomers, avgCustomerVolume, hourlyVolume, dailyVolume) {
+var Store = function(site, minCustomers, maxCustomers, avgCustomerVolume) {
   this.site = site;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
@@ -8,23 +8,22 @@ var Store = function(site, minCustomers, maxCustomers, avgCustomerVolume, hourly
 };
 
 Store.prototype.calcHourlyVolume = function () {
-  // for (var i = 0; i < hours.length; i++) {
-  //  // fix broken math
-  //  x = Math.floor((Math.random() * (store.maxCustomers - store.minCustomers + 1) + store.minCustomers) * store.avgCustomerVolume);
-  //  // x = Math.floor(((Math.random() * (store.maxCustomers - store.minCustomers + 1)) + store.minCustomers) * store.avgCustomerVolume);
-  //  store.hourlyVolume[i] = x;
-  // }
+  var hourlyVolume = [];
+  hourlyVolume = [1,2,3,4,5,6,7,8];
+  return hourlyVolume;
 };
 
 Store.prototype.calcDailyVolume = function() {
-  // var calcDailyVolume = function(store) {
-  //   for (var i = 0; i < hours.length; i++) {
-  //     store.dailyVolume += store.hourlyVolume[i];
-  //   }
-  // };
+  var dailyVolume = 0;
+  var n = [];
+  n = this.calcHourlyVolume();
+  n.forEach(function(y) {
+    dailyVolume = dailyVolume + y;
+  });
+  return dailyVolume;
 };
 
-Store.prototype.addRow = function () {
+Store.prototype.renderStore = function () {
   // var trEl = document.createElement('tr');
   // var tdEl = document.createElement('td');
   // tdEl.appendChild(document.createTextNode(allStores[i].site));
@@ -46,39 +45,9 @@ var tableEl = document.getElementById('sales-table');
 var storeForm = document.getElementById('store-form');
 
 var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
-
 var allStores = [];
-addStore('Pike Place Market', 17, 88, 5.2);
-addStore('SeaTac Airport', 6, 44, 1.2);
-addStore('Southcenter Mall', 11, 38, 1.9);
-addStore('Bellevue Square', 20, 48, 3.3);
-addStore('Alki', 3, 24, 26);
 
-// add store
-function addStore(site, minCustomers, maxCustomers, avgCustomerVolume) {
-  var newStore = new Store(site, minCustomers, maxCustomers, avgCustomerVolume);
-  allStores.push(newStore);
-  //allStores[allStores.length -1].addRow;
-};
-
-// event handler
-var handleStoreSubmit = function(event) {
-  event.preventDefault();
-
-  var site = event.target.site.value;
-  var minCustomers = parseInt(event.target.minCustomers.value, 10);
-  var maxCustomers = parseInt(event.target.maxCustomers.value, 10);
-  var avgCustomerVolume = parseInt(event.target.avgCustomerVolume.value, 10);
-
-  addStore(site, minCustomers, maxCustomers, avgCustomerVolume);
-
-  event.target.site.value = null;
-  event.target.minCustomers.value = null;
-  event.target.maxCustomers.value = null;
-  event.target.avgCustomerVolume.value = null;
-};
-
-// add table head
+// render table head
 (function() {
   var theadEl = document.createElement('thead');
   var trEl = document.createElement('tr');
@@ -97,7 +66,36 @@ var handleStoreSubmit = function(event) {
   tableEl.appendChild(theadEl);
 })();
 
-//
+// add store
+var addStore = function(site, minCustomers, maxCustomers, avgCustomerVolume) {
+  var newStore = new Store(site, minCustomers, maxCustomers, avgCustomerVolume);
+  allStores.push(newStore);
+  //allStores[allStores.length -1].renderStore;
+};
+
+// add initial stores
+addStore('Pike Place Market', 17, 88, 5.2);
+addStore('SeaTac Airport', 6, 44, 1.2);
+addStore('Southcenter Mall', 11, 38, 1.9);
+addStore('Bellevue Square', 20, 48, 3.3);
+addStore('Alki', 3, 24, 26);
+
+// event handler
+var handleStoreSubmit = function(event) {
+  event.preventDefault();
+
+  var site = event.target.site.value;
+  var minCustomers = parseInt(event.target.minCustomers.value, 10);
+  var maxCustomers = parseInt(event.target.maxCustomers.value, 10);
+  var avgCustomerVolume = parseInt(event.target.avgCustomerVolume.value, 10);
+
+  addStore(site, minCustomers, maxCustomers, avgCustomerVolume);
+
+  event.target.site.value = null;
+  event.target.minCustomers.value = null;
+  event.target.maxCustomers.value = null;
+  event.target.avgCustomerVolume.value = null;
+};
 
 // event listener
 storeForm.addEventListener('submit', handleStoreSubmit);
