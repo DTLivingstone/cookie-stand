0f59ff9b8ec73ad1424f64dabc066b9ba597658b
David Livingstone <david@david.tl>
2015-12-16 01:44:11 -0800

    major refactor in progress, only table head is rendering now

--- js/sales.js
+++ js/sales.js
@@ -1,110 +1,103 @@
-// constructor for individual store
+// constructor and prototypes
+
 var Store = function(site, minCustomers, maxCustomers, avgCustomerVolume, hourlyVolume, dailyVolume) {
   this.site = site;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCustomerVolume = avgCustomerVolume;
-  this.hourlyVolume = hourlyVolume;
-  this.dailyVolume = dailyVolume;
+};
+
+Store.prototype.calcHourlyVolume = function () {
+  // for (var i = 0; i < hours.length; i++) {
+  //  // fix broken math
+  //  x = Math.floor((Math.random() * (store.maxCustomers - store.minCustomers + 1) + store.minCustomers) * store.avgCustomerVolume);
+  //  // x = Math.floor(((Math.random() * (store.maxCustomers - store.minCustomers + 1)) + store.minCustomers) * store.avgCustomerVolume);
+  //  store.hourlyVolume[i] = x;
+  // }
+};
+
+Store.prototype.calcDailyVolume = function() {
+  // var calcDailyVolume = function(store) {
+  //   for (var i = 0; i < hours.length; i++) {
+  //     store.dailyVolume += store.hourlyVolume[i];
+  //   }
+  // };
+};
+
+Store.prototype.addRow = function () {
+  // var trEl = document.createElement('tr');
+  // var tdEl = document.createElement('td');
+  // tdEl.appendChild(document.createTextNode(allStores[i].site));
+  // trEl.appendChild(tdEl);
+  // var tdEl = document.createElement('td');
+  // tdEl.appendChild(document.createTextNode(allStores[i].dailyVolume));
+  // trEl.appendChild(tdEl);
+  // for(var j =  0; j < hours.length; j++ ){
+  //   var tdEl = document.createElement('td');
+  //   tdEl.appendChild(document.createTextNode(allStores[i].hourlyVolume[j]));
+  //   trEl.appendChild(tdEl);
+  // }
+  // tableEl.appendChild(trEl);
+  // el.appendChild(tableEl);
 };
 
 // initialize variables
-var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
-var el = document.getElementById('salesTable');
-var tableEl = document.createElement('table');
-var storeform = document.getElementById('store-form');
-var allStores = [];
-var newStore = new Store('Pike Place Market', 17, 88, 5.2, [], 0);
-allStores.push(newStore);
-var newStore = new Store('SeaTac Airport', 6, 44, 1.2, [], 0);
-allStores.push(newStore);
-var newStore = new Store('Southcenter Mall', 11, 38, 1.9, [], 0);
-allStores.push(newStore);
-var newStore = new Store('Bellevue Square', 20, 48, 3.3, [], 0);
-allStores.push(newStore);
-var newStore = new Store('Alki', 3, 24, 26, [], 0);
-allStores.push(newStore);
+var tableEl = document.getElementById('sales-table');
+var storeForm = document.getElementById('store-form');
 
-// functions
-var calcHourlyVolume =  function(store) {
-   for (var i = 0; i < hours.length; i++) {
-    // TODO fix broken math
-    // x = Math.floor((Math.random() * (store.maxCustomers - store.minCustomers + 1) + store.minCustomers) * store.avgCustomerVolume);
-    x = Math.floor(((Math.random() * (store.maxCustomers - store.minCustomers + 1)) + store.minCustomers) * store.avgCustomerVolume);
-    store.hourlyVolume[i] = x;
-  }
+var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
+
+var allStores = [];
+addStore('Pike Place Market', 17, 88, 5.2);
+addStore('SeaTac Airport', 6, 44, 1.2);
+addStore('Southcenter Mall', 11, 38, 1.9);
+addStore('Bellevue Square', 20, 48, 3.3);
+addStore('Alki', 3, 24, 26);
+
+// add store
+function addStore(site, minCustomers, maxCustomers, avgCustomerVolume) {
+  var newStore = new Store(site, minCustomers, maxCustomers, avgCustomerVolume);
+  allStores.push(newStore);
+  //allStores[allStores.length -1].addRow;
 };
-var calcDailyVolume = function(store) {
-  for (var i = 0; i < hours.length; i++) {
-    store.dailyVolume += store.hourlyVolume[i];
-  }
-};
-var renderTableHead = function() {
-  var theadEl = document.createElement('thead');
-  var trEl = document.createElement('tr');
-  var tdEl = document.createElement('td');
-  tdEl.appendChild(document.createTextNode('Location'));
-  trEl.appendChild(tdEl);
-  var tdEl = document.createElement('td');
-  tdEl.appendChild(document.createTextNode('Total'));
-  trEl.appendChild(tdEl);
-  for (var i = 0; i < hours.length; i++) {
-    var tdEl = document.createElement('td');
-    tdEl.appendChild(document.createTextNode(hours[i]));
-    trEl.appendChild(tdEl);
-  }
-  theadEl.appendChild(trEl);
-  tableEl.appendChild(theadEl);
-  el.appendChild(tableEl);
-};
-var renderTableRows = function() {
-  for (var i = 0; i < allStores.length ; i++) {
-    var trEl = document.createElement('tr');
-    var tdEl = document.createElement('td');
-    tdEl.appendChild(document.createTextNode(allStores[i].site));
-    trEl.appendChild(tdEl);
-    var tdEl = document.createElement('td');
-    tdEl.appendChild(document.createTextNode(allStores[i].dailyVolume));
-    trEl.appendChild(tdEl);
-    for(var j =  0; j < hours.length; j++ ){
-      var tdEl = document.createElement('td');
-      tdEl.appendChild(document.createTextNode(allStores[i].hourlyVolume[j]));
-      trEl.appendChild(tdEl);
-    }
-    tableEl.appendChild(trEl);
-    el.appendChild(tableEl);
-  }
-};
+
+// event handler
 var handleStoreSubmit = function(event) {
   event.preventDefault();
-  clearTable();
+
   var site = event.target.site.value;
   var minCustomers = parseInt(event.target.minCustomers.value, 10);
   var maxCustomers = parseInt(event.target.maxCustomers.value, 10);
-  var avgCustomerVolume = parseInt(event.target.avgCustomerVolume.value);
-  var newStore = new Store(site, minCustomers, maxCustomers, avgCustomerVolume, [], 0);
-  console.log(newStore);
+  var avgCustomerVolume = parseInt(event.target.avgCustomerVolume.value, 10);
+
+  addStore(site, minCustomers, maxCustomers, avgCustomerVolume);
+
   event.target.site.value = null;
   event.target.minCustomers.value = null;
   event.target.maxCustomers.value = null;
   event.target.avgCustomerVolume.value = null;
-
-  allStores.push(newStore);
-  allStores.forEach(calcHourlyVolume);
-  allStores.forEach(calcDailyVolume);
-  renderTableHead();
-  renderTableRows();
-};
-var clearTable = function() {
-  console.log("removing: " + el.firstChild);
-  el.removeChild(el.firstChild);
 };
 
-// populate table with initial data
-allStores.forEach(calcHourlyVolume);
-allStores.forEach(calcDailyVolume);
-renderTableHead();
-renderTableRows();
+// add table head
+(function() {
+  var theadEl = document.createElement('thead');
+  var trEl = document.createElement('tr');
+
+  var renderTh = function(text) {
+    var thEl = document.createElement('th');
+    thEl.appendChild(document.createTextNode(text));
+    trEl.appendChild(thEl);
+  }
+
+  renderTh("Location");
+  renderTh("Total");
+  hours.forEach(renderTh);
+
+  theadEl.appendChild(trEl);
+  tableEl.appendChild(theadEl);
+})();
+
+//
 
 // event listener
 storeForm.addEventListener('submit', handleStoreSubmit);
