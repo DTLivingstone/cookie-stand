04def0a560feaf74d16086c4ff485964d3ffe54a
David Livingstone <david@david.tl>
2015-12-16 22:07:44 -0800

    completed assignment

--- js/sales.js
+++ js/sales.js
@@ -1,57 +1,45 @@
 // constructor and prototypes
-var Store = function(site, minCustomers, maxCustomers, avgCustomerVolume) {
+var Store = function(site, minCustomers, maxCustomers, avgCustomerVolume, hourlyVolume, dailyVolume) {
   this.site = site;
   this.minCustomers = minCustomers;
   this.maxCustomers = maxCustomers;
   this.avgCustomerVolume = avgCustomerVolume;
+  this.hourlyVolume = hourlyVolume;
+  this.dailyVolume = dailyVolume;
 };
 
-Store.prototype.calcHourlyVolume = function () {
+Store.prototype.calcHourlyVolume = function() {
   var hourlyVolume = [];
   for (var i = 0; i < hours.length; i += 1) {
-    hourlyVolume[i] = Math.floor(Math.random() * 9);
-    // randomization code
-
+    hourlyVolume[i] = Math.round(this.avgCustomerVolume * (Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1)) + this.minCustomers));
   }
-  return hourlyVolume;
+  this.hourlyVolume = hourlyVolume;
 };
 
 Store.prototype.calcDailyVolume = function() {
   var dailyVolume = 0;
-  this.calcHourlyVolume().forEach(function(y) {
+  this.hourlyVolume.forEach(function(y) {
     dailyVolume = dailyVolume + y;
   });
-  return dailyVolume;
+  this.dailyVolume = dailyVolume;
 };
 
-Store.prototype.renderStore = function () {
-  // make new row
-  // append site as td
-  // append total volume as td
-  // append hourly volumes as tds
-  // append row to table
-
-
+Store.prototype.renderStore = function() {
   var trEl = document.createElement('tr');
-  var tdEl = document.createElement('td');
-  // tdEl.appendChild(document.createTextNode(allStores[i].site));
-  // trEl.appendChild(tdEl);
-  // var tdEl = document.createElement('td');
-  // tdEl.appendChild(document.createTextNode(allStores[i].dailyVolume));
-  // trEl.appendChild(tdEl);
-  // for(var j =  0; j < hours.length; j++ ){
-  //   var tdEl = document.createElement('td');
-  //   tdEl.appendChild(document.createTextNode(allStores[i].hourlyVolume[j]));
-  //   trEl.appendChild(tdEl);
-  // }
-  // tableEl.appendChild(trEl);
-  // el.appendChild(tableEl);
+  var renderTh = function(text){
+    var tdEl = document.createElement('td');
+    tdEl.appendChild(document.createTextNode(text));
+    trEl.appendChild(tdEl);
+  };
+  renderTh(this.site);
+  renderTh(this.dailyVolume);
+  this.hourlyVolume.forEach(renderTh);
+  tableEl.appendChild(trEl);
 };
 
 // initialize variables
 var tableEl = document.getElementById('sales-table');
 var storeForm = document.getElementById('store-form');
-
 var hours = ['10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm'];
 var allStores = [];
 
@@ -59,17 +47,14 @@ var allStores = [];
 (function() {
   var theadEl = document.createElement('thead');
   var trEl = document.createElement('tr');
-
   var renderTh = function(text) {
     var thEl = document.createElement('th');
     thEl.appendChild(document.createTextNode(text));
     trEl.appendChild(thEl);
   }
-
   renderTh("Location");
   renderTh("Total");
   hours.forEach(renderTh);
-
   theadEl.appendChild(trEl);
   tableEl.appendChild(theadEl);
 })();
@@ -78,7 +63,9 @@ var allStores = [];
 var addStore = function(site, minCustomers, maxCustomers, avgCustomerVolume) {
   var newStore = new Store(site, minCustomers, maxCustomers, avgCustomerVolume);
   allStores.push(newStore);
-  //allStores[allStores.length -1].renderStore;
+  allStores[allStores.length - 1].calcHourlyVolume();
+  allStores[allStores.length - 1].calcDailyVolume();
+  allStores[allStores.length - 1].renderStore();
 };
 
 // add initial stores
@@ -91,14 +78,11 @@ addStore('Alki', 3, 24, 26);
 // event handler
 var handleStoreSubmit = function(event) {
   event.preventDefault();
-
   var site = event.target.site.value;
   var minCustomers = parseInt(event.target.minCustomers.value, 10);
   var maxCustomers = parseInt(event.target.maxCustomers.value, 10);
   var avgCustomerVolume = parseInt(event.target.avgCustomerVolume.value, 10);
-
   addStore(site, minCustomers, maxCustomers, avgCustomerVolume);
-
   event.target.site.value = null;
   event.target.minCustomers.value = null;
   event.target.maxCustomers.value = null;
